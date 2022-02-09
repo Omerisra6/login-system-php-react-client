390ce19854c7f1bc328885fe31e6ddd2426847f2
omer <omer.israeli@boyar.org.il>
2022-02-09 22:05:30 +0200

    Fix: Main wrapper margin and background fixed

---
+++ client/php-login-client/src/ui/Styled/StyledMain.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const StyledMain = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: #ffff;
+    margin: 10vh 5vw;
+`
+
+export default StyledMain
--- client/php-login-client/src/ui/Styled/StyledTopMain.js
+++ client/php-login-client/src/ui/Styled/StyledTopMain.js
@@ -4,9 +4,7 @@ const StyledTopMain = styled.div`
     display: flex;
     justify-content: space-between;
     padding-left: 1vw;
-    margin: 1vh 2vw;
     background-color: #ffff;
-
 `
 
 export default StyledTopMain
