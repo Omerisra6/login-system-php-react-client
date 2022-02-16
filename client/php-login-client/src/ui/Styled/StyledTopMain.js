dc03dfa07a7bd758d5b9191c25858c4d4015ea5c
omer <omer.israeli@boyar.org.il>
2022-02-16 16:16:00 +0200

    New: UI chnages in top main component

--- client/php-login-client/src/ui/Styled/StyledTopMain.js
+++ client/php-login-client/src/ui/Styled/StyledTopMain.js
@@ -5,6 +5,7 @@ const StyledTopMain = styled.div`
     justify-content: space-between;
     padding-left: 1vw;
     background-color: #ffff;
+    border-bottom: 2px solid #F2F3F7;
 `
 
 export default StyledTopMain
